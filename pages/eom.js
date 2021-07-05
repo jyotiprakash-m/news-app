7ec5a5a4f4ee82893552212e9694f6dbb2d87e0e
jyotiprakash-m <teachmeonline365@gmail.com>
2021-07-05 19:04:25 +0530

    almost complete

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,37 @@
 import React from 'react'
-
+import styles from '../styles/EOM.module.css';
+import { Toolbar } from '../components/toolbar';
 function EMO({ employee }) {
+    console.log(employee)
     return (
-        <div>
-            Hello World
+        <div className="page-container">
+            <Toolbar />
+
+            <div className={styles.main}>
+                <h1>Employee Of The Month</h1>
+
+                <div className={styles.employeeOfTheMonth}>
+                    <h3>{employee.name}</h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image} alt="employee" />
+                    <p>{employee.description}</p>
+                </div>
+            </div>
         </div>
     )
+}
+
+// Server Side Props
+export const getServerSideProps = async pageContext => {
+    const response = await fetch('https://my-json-server.typicode.com/jyotiprakash-m/news-app/employeeOfTheMonth');
+
+    const employee = await response.json();
+
+    return {
+        props: {
+            employee
+        }
+    }
 }
 
 
