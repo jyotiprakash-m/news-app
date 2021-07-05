ab9c83105d8baca3d38fcb875c562090e10e5b37
jyotiprakash-m <teachmeonline365@gmail.com>
2021-07-05 19:16:00 +0530

    seo added

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Head from "next/head"
 import { useRouter } from 'next/router';
 import styles from '../../styles/Feed.module.css';
 import { Toolbar } from '../../components/toolbar';
@@ -6,44 +6,51 @@ function Feed({ pageNumber, articles }) {
     const router = useRouter()
     console.log(articles, pageNumber)
     return (
-        <div className="page-container">
-            <Toolbar />
+        <>
+            <Head>
+                <meta property="og:image" content={articles[0]?.urlToImage} />
+                <meta property="og:description" content={articles[0]?.description} />
+                <meta property="og:title" content={articles[0]?.title + ' and more!'} />
+            </Head>
+            <div className="page-container">
+                <Toolbar />
 
-            <div className={styles.main}>
-                {articles.map((article, index) => (
-                    <div key={index} className={styles.post}>
-                        <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
-                        <p>{article.description}</p>
-                        {!!article.urlToImage && <img src={article.urlToImage} />}
+                <div className={styles.main}>
+                    {articles.map((article, index) => (
+                        <div key={index} className={styles.post}>
+                            <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+                            <p>{article.description}</p>
+                            {!!article.urlToImage && <img src={article.urlToImage} />}
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.paginator}>
+                    <div
+                        className={pageNumber === 1 ? styles.disabled : styles.active}
+                        onClick={() => {
+                            if (pageNumber > 1) {
+                                router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+                            }
+                        }}
+                    >
+                        Previous Page
                     </div>
-                ))}
-            </div>
-            <div className={styles.paginator}>
-                <div
-                    className={pageNumber === 1 ? styles.disabled : styles.active}
-                    onClick={() => {
-                        if (pageNumber > 1) {
-                            router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
-                        }
-                    }}
-                >
-                    Previous Page
-                </div>
 
-                <div>#{pageNumber}</div>
+                    <div>#{pageNumber}</div>
 
-                <div
-                    className={pageNumber === 5 ? styles.disabled : styles.active}
-                    onClick={() => {
-                        if (pageNumber < 5) {
-                            router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
-                        }
-                    }}
-                >
-                    Next Page
+                    <div
+                        className={pageNumber === 5 ? styles.disabled : styles.active}
+                        onClick={() => {
+                            if (pageNumber < 5) {
+                                router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+                            }
+                        }}
+                    >
+                        Next Page
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
